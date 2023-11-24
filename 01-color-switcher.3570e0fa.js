1cba44800ed3763d7e1505915830df908418ad32
romahalaichuk <125358061+romahalaichuk@users.noreply.github.com>
2023-11-24 10:03:33 +0000

    Deploying to gh-pages from @ romahalaichuk/goit-js-hw-09@0a77eea334fab356f46e1caf63bce6030b9d1fab 🚀

---
+++ 01-color-switcher.3570e0fa.js
@@ -0,0 +1,2 @@
+let t;document.getElementById("startButton").addEventListener("click",function(){this.disabled=!0,document.getElementById("stopButton").disabled=!1,t=setInterval(function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`},1e3)}),document.getElementById("stopButton").addEventListener("click",function(){document.getElementById("startButton").disabled=!1,this.disabled=!0,clearInterval(t)});
+//# sourceMappingURL=01-color-switcher.3570e0fa.js.map
